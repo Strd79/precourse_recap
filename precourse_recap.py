b3d68c688c33ec32fa769d26b3bbdec143ffed19
user <user@macbook-pro.home>
2020-08-31 17:20:32 +0100

    Updated code to include input() option, as forgot in class

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,10 +1,10 @@
-lucky_number = 8
+lucky_number = int(input("Guess David's lucky number? "))
 
 if lucky_number == 8:
-    print("This is David's lucky number")
+    print("This IS David's lucky number")
 elif lucky_number == 4:
     print("4 is an unlucky number!")
 else:
-    print("This is not David's lucky number")
+    print("This is NOT David's lucky number")
 
 
