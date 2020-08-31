bce4d5252d69642b1af42871dc578d96498027df
user <user@users-MacBook-Pro-5.local>
2020-08-31 15:26:11 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,10 @@
+lucky_number = 8
+
+if lucky_number == 8:
+    print("This is David's lucky number")
+elif lucky_number == 4:
+    print("4 is an unlucky number!")
+else:
+    print("This is not David's lucky number")
+
+
